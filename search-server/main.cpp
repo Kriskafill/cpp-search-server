c00daff3321e9bc90fda0758ac009ecf1f9895f8
Kriskafill <kriska.fill@yandex.ru>
2023-11-21 23:28:38 +0300

    Sprint 0

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -8,10 +8,12 @@ using namespace std;
 int main() {
 
 	int count = 0;
+
 	for (int i = 1; i <= 1000; ++i)
 	{
 		if (i % 10 == 3 || (i % 100) / 10 == 3 || i / 100 == 3) ++count;
 	}
+	
 	cout << count << endl;
 }
 

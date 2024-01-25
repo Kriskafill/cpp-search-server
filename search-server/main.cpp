1f1ae39fd7a6a6503fd4367a825a3cd4fb48f95a
Kriskafill <kriska.fill@yandex.ru>
2024-01-25 08:43:12 +0300

    Sprint 4: Education Version (Deque)

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <cmath>
+#include <deque>
 #include <iostream>
 #include <map>
 #include <numeric>
@@ -7,7 +8,7 @@
 #include <string>
 #include <vector>
 
-// Sprint 4: Education Version (Iterators)
+// Sprint 4: Education Version (Deque)
 
 using namespace std;
 
@@ -371,6 +372,52 @@ private:
     vector<IteratorRange<Iterator>> pages_;
 };
 
+class RequestQueue {
+public:
+
+    explicit RequestQueue(const SearchServer& search_server) : search_server_(search_server) { }
+    
+    template <typename DocumentPredicate>
+    vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate document_predicate) {
+        if (requests_.size() == min_in_day_) {
+            requests_.pop_back();
+        }
+
+        const QueryResult result = { raw_query, search_server_.FindTopDocuments(raw_query, document_predicate) };
+        requests_.push_front(result);
+
+        return result.result;
+    }
+
+    vector<Document> AddFindRequest(const string& raw_query) {
+        if (requests_.size() == min_in_day_) {
+            requests_.pop_back();
+        }
+
+        const QueryResult result = { raw_query, search_server_.FindTopDocuments(raw_query) };
+        requests_.push_front(result);
+
+        return result.result;
+    }
+
+    int GetNoResultRequests() const {
+        return count_if(requests_.begin(), requests_.end(), [](const QueryResult& query) {
+            return query.result.empty();
+        });
+    }
+
+private:
+
+    struct QueryResult {
+        string query;
+        vector<Document> result;
+    };
+
+    deque<QueryResult> requests_;
+    const SearchServer& search_server_;
+    const static int min_in_day_ = 1440;
+};
+
 ostream& operator<<(ostream& output, const Document& document) {
     output << "{ "s
          << "document_id = "s << document.id << ", "s
@@ -396,19 +443,22 @@ auto Paginate(const Container& c, size_t page_size) {
 }
 
 int main() {
-    SearchServer search_server("and with"s);
-    search_server.AddDocument(1, "funny pet and nasty rat"s, DocumentStatus::ACTUAL, { 7, 2, 7 });
-    search_server.AddDocument(2, "funny pet with curly hair"s, DocumentStatus::ACTUAL, { 1, 2, 3 });
-    search_server.AddDocument(3, "big cat nasty hair"s, DocumentStatus::ACTUAL, { 1, 2, 8 });
-    search_server.AddDocument(4, "big dog cat Vladislav"s, DocumentStatus::ACTUAL, { 1, 3, 2 });
-    search_server.AddDocument(5, "big dog hamster Borya"s, DocumentStatus::ACTUAL, { 1, 1, 1 });
-
-    const auto search_results = search_server.FindTopDocuments("curly dog"s);
-    int page_size = 2;
-    const auto pages = Paginate(search_results, page_size);
+    SearchServer search_server("and in at"s);
+    RequestQueue request_queue(search_server);
+    search_server.AddDocument(1, "curly cat curly tail"s, DocumentStatus::ACTUAL, { 7, 2, 7 });
+    search_server.AddDocument(2, "curly dog and fancy collar"s, DocumentStatus::ACTUAL, { 1, 2, 3 });
+    search_server.AddDocument(3, "big cat fancy collar "s, DocumentStatus::ACTUAL, { 1, 2, 8 });
+    search_server.AddDocument(4, "big dog sparrow Eugene"s, DocumentStatus::ACTUAL, { 1, 3, 2 });
+    search_server.AddDocument(5, "big dog sparrow Vasiliy"s, DocumentStatus::ACTUAL, { 1, 1, 1 });
     
-    for (auto page = pages.begin(); page != pages.end(); ++page) {
-        cout << *page << endl;
-        cout << "Page break"s << endl;
+    for (int i = 0; i < 1439; ++i) {
+        request_queue.AddFindRequest("empty request"s);
     }
+    request_queue.AddFindRequest("curly dog"s);
+    request_queue.AddFindRequest("big collar"s);
+    request_queue.AddFindRequest("sparrow"s);
+
+    cout << "Total empty requests: "s << request_queue.GetNoResultRequests() << endl;
+
+    return 0;
 }
